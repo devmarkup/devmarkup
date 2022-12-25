d982dc560d6151458d01070222ef641f40b486bf
Vitaliy <bets2305@gmail.com>
2022-12-26 01:08:17 +0200

    desktop

--- gci-services/js/app.js
+++ gci-services/js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(() => {
-  console.log('test');
+  function smoothScrollLink() {
+    $('a.smooth-scroll').click(function () {
+      const elementClick = $(this).attr('href');
+      const destination = $(elementClick).offset().top;
+      $('html,body').animate({ scrollTop: destination }, 1000);
+      return false;
+    });
+  }
+
+  smoothScrollLink();
 });
